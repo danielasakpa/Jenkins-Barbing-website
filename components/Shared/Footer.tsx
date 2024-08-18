db8210a0694722a360197ed128ce72eb35caad51
danielasakpa <danielasakpa@gmail.com>
2024-08-18 03:08:47 -0700

    refactor: update Footer component with links and additional social icons
    - Added Link component for navigation and social icons
    - Replaced static social icon spans with Link components
    - Added FaWhatsapp icon for WhatsApp link
    - Updated social media links for Facebook, Instagram, and WhatsApp
    - Replaced header logo with a custom ScissorsIcon
    - Adjusted styles and formatting for better layout

--- components/Shared/Footer.tsx
+++ components/Shared/Footer.tsx
@@ -3,12 +3,14 @@ import Section from "./Section";
 import Image from "next/image";
 import { GrFacebookOption } from "react-icons/gr";
 import { AiFillInstagram } from "react-icons/ai";
-import { FaTwitter } from "react-icons/fa";
+import { FaTwitter, FaWhatsapp } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { IoCallSharp, IoTimeOutline } from "react-icons/io5";
 import { IconType } from "react-icons";
 import { footerContacts } from "@/constants";
+import ScissorsIcon from "./icons/ScissorsIcon";
+import Link from "next/link";
 
 const iconMap: { [key: string]: IconType } = {
   FaLocationDot,
@@ -19,21 +21,25 @@ const iconMap: { [key: string]: IconType } = {
 
 const SocialIcon = ({
   IconComponent,
+  link,
   bgColor,
 }: {
   IconComponent: IconType;
+  link: string;
   bgColor: string;
 }) => (
-  <span className={`p-2 ${bgColor} rounded-md cursor-pointer`}>
+  <Link href={link} className={`p-2 ${bgColor} rounded-md cursor-pointer`}>
     <IconComponent className="text-white" size={20} />
-  </span>
+  </Link>
 );
 
 const ContactItem = ({ icon, title }: { icon: string; title: string }) => {
   const IconComponent = iconMap[icon];
   return (
     <div className="flex gap-3 items-center text-white">
-      {IconComponent && <IconComponent size={15} className="text-[#DC5F00]" />}
+      {IconComponent && (
+        <IconComponent size={15} className="text-[#DC5F00] mb-3" />
+      )}
       <span>{title}</span>
     </div>
   );
@@ -52,50 +58,62 @@ const ImageItem = ({ src, alt }: { src: string; alt: string }) => (
 );
 
 const Footer = () => (
-  <div className="w-full max-w-full bg-black">
-    <Section className="py-20 px-6 md:px-20 mx-auto grid grid-cols-1 md:grid-cols-3 justify-items-center content-start gap-20 text-center">
-      <div className="text-left flex flex-col justify-start h-full">
-        <a className="flex gap-2 items-center w-[12rem]" href="#hero">
-          <Image
-            src={"/header-logo.png"}
-            width={50}
-            height={40}
-            alt="header-logo"
+  <Section className="w-full max-w-full bg-black !py-[50px] px-6 md:px-20 !mb-0 mx-auto grid grid-cols-1 md:grid-cols-3 justify-items-center content-start gap-20 text-center">
+    <div className="text-left flex flex-col justify-start h-full">
+      <Link
+        href="/"
+        className="mb-2 flex items-center text-white"
+        prefetch={false}
+      >
+        <ScissorsIcon className="w-7 h-7" />
+        <span className="p-medium-20 lg:p-medium-24 text-white">JENKINS</span>
+      </Link>
+      <span className="block mt-2 text-white opacity-80">
+        We create styles for the way you look and the way you live!
+      </span>
+      <div className="flex gap-3 items-center mt-4">
+        <SocialIcon
+          IconComponent={GrFacebookOption}
+          bgColor="bg-blue-600"
+          link={"https://wa.me/message/Y7GCET35NVEYI1 "}
+        />
+        <SocialIcon
+          IconComponent={FaWhatsapp}
+          bgColor="bg-green-600"
+          link={"https://wa.me/message/Y7GCET35NVEYI1"}
+        />
+
+        <SocialIcon
+          IconComponent={AiFillInstagram}
+          bgColor="bg-pink-700"
+          link={
+            "https://www.instagram.com/jenkins_haircut?utm_source=qr&igsh=Yno0OWN3Y3h2dTJo"
+          }
+        />
+      </div>
+    </div>
+    <div className="text-left flex flex-col justify-start h-full">
+      <span className="block p-medium-20 text-white mb-3">Contact Us</span>
+      <div className="flex flex-col gap-3">
+        {footerContacts.map((contact) => (
+          <ContactItem
+            key={contact.id}
+            icon={contact.icon}
+            title={contact.title}
           />
-          <span className="h4-medium text-white">JENKINS</span>
-        </a>
-        <span className="block mt-2 text-white opacity-80">
-          We create styles for the way you look and the way you live!
-        </span>
-        <div className="flex gap-3 items-center mt-4">
-          <SocialIcon IconComponent={GrFacebookOption} bgColor="bg-blue-600" />
-          <SocialIcon IconComponent={AiFillInstagram} bgColor="bg-pink-500" />
-          <SocialIcon IconComponent={FaTwitter} bgColor="bg-blue-400" />
-        </div>
+        ))}
       </div>
-      <div className="text-left flex flex-col justify-start h-full">
-        <span className="block p-medium-20 text-white mb-3">Contact Us</span>
-        <div className="flex flex-col gap-3">
-          {footerContacts.map((contact) => (
-            <ContactItem
-              key={contact.id}
-              icon={contact.icon}
-              title={contact.title}
-            />
-          ))}
-        </div>
+    </div>
+    <div className="hidden text-left md:flex flex-col items-start h-full">
+      <span className="block p-medium-20 text-white mb-3">Photo Gallery</span>
+      <div className="grid grid-cols-2 gap-4">
+        <ImageItem src="/about.jpg" alt="About Image" />
+        <ImageItem src="/about1.jpg" alt="About Image 1" />
+        <ImageItem src="/about2.jpg" alt="About Image 2" />
+        <ImageItem src="/about3.jpg" alt="About Image 3" />
       </div>
-      <div className="hidden text-left md:flex flex-col items-start h-full">
-        <span className="block p-medium-20 text-white mb-3">Photo Gallery</span>
-        <div className="grid grid-cols-2 gap-4">
-          <ImageItem src="/about.jpg" alt="About Image" />
-          <ImageItem src="/about1.jpg" alt="About Image 1" />
-          <ImageItem src="/about2.jpg" alt="About Image 2" />
-          <ImageItem src="/about3.jpg" alt="About Image 3" />
-        </div>
-      </div>
-    </Section>
-  </div>
+    </div>
+  </Section>
 );
 
 export default Footer;
