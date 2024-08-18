90f89a467c054686cb061bf30ddca6ea5bb9e623
danielasakpa <danielasakpa@gmail.com>
2024-08-18 03:31:07 -0700

    fix: Escaped single quotes and other entities in JSX to adhere to `react/no-unescaped-entities` rule.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
               Revitalize your senses and Refresh your mind.
             </h1>
             <p className="mt-4 p-regular-18">
-              At JenkinsIt&apos;s Haircut, we offer more than just a haircut.
+              At Jenkins&apos;s Haircut, we offer more than just a haircut.
               Refresh your mind and revitalize your senses in a relaxing
               atmosphere, leaving you looking sharp and feeling renewed.
             </p>
@@ -41,8 +41,8 @@ export default function Home() {
           <span className="text-[#DC5F00] mb-3">TESTIMONIALS</span>
           <h1 className="h1-bold">Hear from our customers</h1>
           <p className="mt-4 p-regular-18">
-            Don't just take our word for it—hear what our clients have to say
-            about their experience at JenkinsIt&apos;s Haircut
+            Don&apos;t just take our word for it—hear what our clients have to
+            say about their experience at Jenkins&apos;s Haircut
           </p>
         </div>
         <InfiniteMovingCardsDemo />
