36776c038bc6b4fc2b0750481bc323844d760499
danielasakpa <danielasakpa@gmail.com>
2024-08-22 15:50:06 -0700

    pref: enhance service card with background image
    - Updated the `ServicesCard` component to use the `Service` type.
    - Added dynamic background images to the service cards using `service.imageUrl`.
    - Retained existing styles and layout while enhancing visual appeal.

--- components/Shared/ServicesCard.tsx
+++ components/Shared/ServicesCard.tsx
@@ -2,18 +2,16 @@ import React from "react";
 import "../../app/(root)/card.css";
 import Button from "./Button";
 import Link from "next/link";
+import { Service } from "@/types";
 
-interface ServicesCardProps {
-  id: string;
-  title: string;
-  desc: string;
-}
-
-const ServicesCard = ({ service }: { service: ServicesCardProps }) => {
+const ServicesCard = ({ service }: { service: Service }) => {
   return (
-    <div className="card">
+    <div
+      className="card bg-cover bg-center"
+      style={{ backgroundImage: `url(${service.imageUrl})` }}
+    >
       <div className="content">
-        <span className="title p-semibold-20 text1">{service.title}</span>
+        <span className="title p-semibold-20 text1">{service.service}</span>
         <p className="mt-6 p-regular-14 hidden lg:block">{service.desc}</p>
         <Link href="/appointment">
           <Button className="btn">Book Now</Button>
