e07547f5283015b78d732000944b10e66cd1dffd
danielasakpa <danielasakpa@gmail.com>
2024-08-28 09:01:52 -0700

    style: update styles

--- components/Shared/Section.tsx
+++ components/Shared/Section.tsx
@@ -8,7 +8,7 @@ interface SectionProps {
 const Section = ({ children, className }: SectionProps) => {
   return (
     <section
-      className={`mx-auto max-w-[1366px] py-[20px] mt-2 mb-16 lg:mb-0 lg:mt-14 md:py-8 ${className}`}
+      className={`mx-auto max-w-[1366px] py-[20px] mt-2 mb-20 lg:mb-0 lg:mt-14 md:py-8 ${className}`}
     >
       {children}
     </section>
